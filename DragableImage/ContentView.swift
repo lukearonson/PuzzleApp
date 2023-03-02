a0615dbe0536db469116f5b21f9abdd09bfd1daa
lukearonson <126193307+lukearonson@users.noreply.github.com>
2023-03-02 09:47:47 -0800

    Part 2

--- DragableImage/ContentView.swift
+++ DragableImage/ContentView.swift
@@ -9,42 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                DragView(imageName: "image1")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-                DragView(imageName: "image2")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-                DragView(imageName: "image3")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
+            ZStack {
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(-120, -120))
+                
+                DragView(imageName: "image2",
+                endLocation: CGPointMake(-0, -120))
+                
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(120, -120))
+                
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(-120, 0))
+                
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(0, 0))
+                
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(120, 0))
+                
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(-120, 120))
+                
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(0, 120))
+                
+                DragView(imageName: "image1",
+                endLocation: CGPointMake(120, 120))
+                   
+               
             }
-            HStack {
-                DragView(imageName: "image4")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-                DragView(imageName: "image5")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-                DragView(imageName: "image6")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-
-            }
-            HStack {
-                DragView(imageName: "image7")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-                DragView(imageName: "image8")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-                DragView(imageName: "image9")
-                    .position(x: CGFloat.random(in: 0...UIScreen.main.bounds.width-300),
-                              y: CGFloat.random(in: 0...UIScreen.main.bounds.height-400))
-            }
-        }
     }
 }
 
--- DragableImage/DragView.swift
+++ DragableImage/DragView.swift
@@ -11,12 +11,32 @@ struct DragView: View {
     private let imageSize: CGFloat = 100
     @State private var offset = CGSize.zero
     var imageName: String
-    
+    var endLocation:CGPoint
     var dragGesture: some Gesture {
         DragGesture()
             .onChanged { value in
-                offset = CGSize(width: value.startLocation.x + value.translation.width - imageSize/2, height: value.startLocation.y + value.translation.height - imageSize/2)
+                
+                var x = value.startLocation.x + value.translation.width - imageSize/2
+                var y = value.startLocation.y + value.translation.height - imageSize/2
+                
+                if abs(x - endLocation.x) < 20 && abs(y - endLocation.y) < 20{
+                    x = endLocation.x
+                    y = endLocation.y
+                }
+                
+                offset = CGSize(
+                    width: x,
+                height: y)
             }
+    }
+    
+    init(imageName: String, endLocation: CGPoint) {
+        self.offset = CGSize(
+            width: Int.random(in: -50...50),
+            height: Int.random(in: -50...50)
+            )
+        self.imageName = imageName
+        self.endLocation = endLocation
     }
     
     var body: some View {
@@ -34,6 +54,6 @@ struct DragView: View {
 
 struct DraggableImage_Previews: PreviewProvider {
     static var previews: some View {
-        DragView(imageName: "your_image_name_here")
+        DragView(imageName: "image", endLocation: CGPointMake(0,0))
     }
 }
